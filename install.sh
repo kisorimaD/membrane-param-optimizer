dbd76fc1205e39685381a45a8f2eabae5f63ae56
kisorimaD <damirosik@gmail.com>
2025-08-06 11:52:55 +0300

    add auto installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+mkdir -p result
+
+apt install libxrender1 libx11-6 libxext6 libxcursor1 libxft2 libxfixes3 libxi6 libglu1-mesa libxinerama1
