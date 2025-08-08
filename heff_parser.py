c7cb2368144d4b392b6a7c934cfd05ab2abfe97a
kisorimaD <damirosik@gmail.com>
2025-08-08 14:20:42 +0300

    heff parser update

--- heff_parser.py
+++ heff_parser.py
@@ -3,6 +3,21 @@ import re
 import subprocess
 from datetime import datetime
 from settings import settings
+from tqdm import tqdm
+import mysql.connector
+
+def grep_lines(lines) -> float:
+    heff = ""
+
+    try:
+        heff = lines[-9].split("Heff =")[1].split(",")[0].strip()
+
+        heff = float(heff)
+    except IndexError as e:
+        print('\n'.join(lines[-30:]))
+        # raise e
+    
+    return heff
 
 directory = 'result/'
 binary_path = settings['MEMBRANEMODEL_PATH'] + '/cmake-build-Release/benchmarks/IdealSuturedLeaf/av_in_cilinder'
@@ -14,7 +29,10 @@ filename_pattern = re.compile(
     r"A_(?P<A>\d+)_P_(?P<P>\d+)_D_(?P<D>\d+)_NCC_closure\.vtk"
 )
 
-for filename in os.listdir(directory):
+
+result = dict()
+
+for filename in tqdm(os.listdir(directory)):
     match = filename_pattern.match(filename)
     if not match:
         continue
@@ -30,9 +48,6 @@ for filename in os.listdir(directory):
     a = match.group("A")
     p = match.group("P")
     d = match.group("D")
-
-    print(f"Обработка файла: {filename}")
-    print(f"A={a}, P={p}, D={d}, mtime={mtime}")
 
     params_raw = f"""
 "--cusp_meshes" "{directory}/{filename}" "" ""
@@ -50,15 +65,108 @@ for filename in os.listdir(directory):
 """
     params = params_raw.strip().replace("\n", " ")
 
-    # Запуск бинарника с аргументами
     try:
-        subprocess.run(
+        av_proc = subprocess.Popen(
             binary_path + " " + params,
             shell=True,
-            check=True
+            stdout=subprocess.PIPE,
+            text=True
         )
-        print(f"  ✔️ Успешно запущено")
+        output, _ = av_proc.communicate()
+        heff = grep_lines(output.splitlines())
+        if heff == '':
+            continue
+        result[(a, p, d)] = heff
+
+
     except subprocess.CalledProcessError as e:
         print(f"  ❌ Ошибка при запуске на {filename}: {e}")
+
+
+user_name = "damir"
+
+MYSQL_CONFIG = {
+    'host': '84.54.47.92',
+    'user': 'user',
+    'password': 'stvorkisirius',
+    'database': 'sim_data'
+}
+
+def create_connection():
+    conn = None
+
+    conn = mysql.connector.connect(**MYSQL_CONFIG)
+
+    return conn
+
+def close_connection(conn):
+    if conn and conn.is_connected():
+        conn.close()
+
+def create_table(conn):
+    sql = f''' 
+    CREATE TABLE IF NOT EXISTS heff_data (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        angle INT NOT NULL,
+        D INT NOT NULL,
+        heff DOUBLE NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        created_by VARCHAR(255) DEFAULT 'unknown'
+    ); '''
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+    
     finally:
-        print("==============\n")
+        if cur:
+            cur.close()
+
+def insert_data(conn, data):
+    sql = f''' 
+    INSERT INTO heff_data(angle, D, heff, created_by)
+    VALUES (%s, %s, %s, %s)
+    '''
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, (data['angle'], data['D'], data['heff'], data['created_by']))
+        conn.commit()
+    finally:
+        if cur:
+            cur.close()
+
+
+def find_data(conn, angle, D):
+    sql = f''' SELECT * FROM heff_data WHERE angle = %s AND D = %s '''
+    try:
+        cur = conn.cursor(buffered=True)
+        cur.execute(sql, (angle, D))
+        result = cur.fetchone()
+        return result
+    finally:
+        if cur:
+            cur.close()
+
+conn = create_connection()
+create_table(conn)
+
+for key, value in result.items():
+    a, p, d = key
+
+    a = float(a)
+    p = int(p)
+    d = int(d)
+
+    if not find_data(conn, a, d):
+
+        print(f"A: {a}, P: {p}, D: {d} -> Heff: {value}")
+
+        insert_data(conn, {
+            'angle': a,
+            'D': d,
+            'heff': value,
+            'created_by': user_name
+        })
+
+        print(f"Insert data for A{a}P{p}D{d}")
+    else:
+        print(f"Data for A{a}P{p}D{d} already exists.")
